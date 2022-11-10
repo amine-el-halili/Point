47115f1ae8963c31b05aa6d6a934616400c78e36
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-10 21:42:25 +0100

    Add: isCollinear method of Point class

--- Point.cs
+++ Point.cs
@@ -33,5 +33,14 @@
 
             return this;
         }
+
+        public bool isCollinear(Point a, Point b)
+        {
+            double area = x * (a.y - b.y) +
+                        a.x * (b.y - y) +
+                        b.x * (y - a.y);
+
+            return (area == 0);
+        }
     }
 }
