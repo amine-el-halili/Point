0ba1dc82f47783a45d7434419cbda302aa10e39b
amine-el-halili <amine.elhalili1@usmba.ac.ma>
2022-11-12 15:36:25 +0100

    Add Circle class

---
+++ Circle.cs
@@ -0,0 +1,40 @@
+﻿namespace Point
+{
+    class Circle : Point
+    {
+        private float radius;
+
+        public Circle(): base()
+        {
+            radius = 0;
+        }
+        public Circle(Point center, float radius): base(center.getX(), center.getY())
+        {
+            this.radius = radius;
+        }
+    
+        public float getRadius()
+        {
+            return radius;
+        }
+        public void setRadius(float radius)
+        {
+            this.radius = radius;
+        }
+
+        public bool isEqual(Circle circle)
+        {
+            return (radius == circle.radius) && (getX() == circle.getX()) && (getY() == circle.getY());
+        }
+
+        public double getCircumference()
+        {
+            return (2 * radius * Math.PI);
+        }
+
+        public double getArea()
+        {
+            return (radius * radius * Math.PI);
+        }
+    }
+}
